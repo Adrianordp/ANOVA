2b7e83c1b764b0251a28e8fd205c693ce4dcf46d
AdrianoI15U <adrianordp@alu.ufc.br>
2019-11-02 17:51:00 -0300

    Initializing a program to work with CSV files

--- anova.py
+++ anova.py
@@ -1,4 +1,5 @@
 # Code beginning --------------------
+import csv
 import sys
 import os
 
@@ -9,11 +10,26 @@ fileName  = sys.argv[1]
 # Create file path
 filePath = root+fileName
 
+fields = []
+rows = []
+
 # Begin logic
-with open(filePath) as outfile:
-    dados = outfile.read().split(' ')
-    print(dados)
-    if '\n' in dados[1]:
-        print('tem')
-    else:
-        print('não tem')
+with open(filePath,'r') as outfile:
+    dados = csv.reader(outfile)
+    fields = csvreader.next();
+    for row in csvreader:
+        rows.append(row)
+
+    # get total number of rows
+    print("Total no. of rows: %d"%(csvreader.line_num))
+
+# printing the field names 
+print('Field names are:' + ', '.join(field for field in fields))
+
+#  printing first 5 rows
+print('\nFirst 5 rows are:\n')
+for row in rows[:5]:
+    # parsing each column of a row
+    for col in row:
+        print("%10s"%col),
+    print('\n')
